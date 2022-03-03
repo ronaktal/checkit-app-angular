3db618baf160ce312b0602f9859294f9ffee05a3
ronaktal <ronaktal@gmail.com>
2022-03-02 23:53:16 -0500

    5 files modified

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { Tab1PageRoutingModule } from './tab1-routing.module';
 
@@ -12,6 +13,7 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
     IonicModule,
     CommonModule,
     FormsModule,
+    HttpClientModule,
     ExploreContainerComponentModule,
     Tab1PageRoutingModule
   ],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
-
-import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  films: Observable<any>;
 
-  constructor(public httpClient: HttpClient) {}
-online
+  online = true;
+
+  constructor(public httpClient: HttpClient, private iab: InAppBrowser) {
+    this.films = this.httpClient.get('https://httpbin.org/');
+    this.films.subscribe(
+      (data) => {
+        this.online = true;
+      },
+      (error) => {
+        this.online = true; //make this false in production
+      }
+    );
+  }
+  open_chat() {
+    var ref = this.iab.create(
+      'https://checkitup.live',
+      '_system',
+      'location=yes'
+    );
+    //    window.open('https://checkitup.live');
+  }
 }
